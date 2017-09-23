4799040f31acd6474334c6482632b91a74de931e
Ksihris <xihris@gmail.com>
2017-09-23 15:35:17 +0300

    department_callbacks

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,2 +1,11 @@
 class Department < ApplicationRecord
+
+  validates :full_department, presence: true
+  validates :full_department, format:{ :with => /[а-яА-Я ]+/ }
+
+  before_save :short
+
+  def short
+    self.short_department = full_department.split(' ').delete_if{ |x| x.length < 3}.map{|x| x.to_s.chars.first}.join('').mb_chars.upcase
+  end
 end
